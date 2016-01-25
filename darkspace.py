92198996f743d228b049514debc745242ac6fbd9
Vishal Lall <vishallall@live.com>
2016-01-25 02:07:04 -0500

    format++

--- darkspace.py
+++ darkspace.py
@@ -10,173 +10,181 @@ from bs4 import BeautifulSoup
 from lxml import html
 from flask import request
 
+
 class BackCheck(object):
-	"""
-	Take user query ex: "John Smith" in darkmain.py and retrieve useful 
-	information from the clearnet,relating it to scraped .onion sites etc. 
-	"""
+    """
+    Take user query ex: "John Smith" in darkmain.py and retrieve useful
+    information from the clearnet,relating it to scraped .onion sites etc.
+    """
 
-	def __init__(self, query, dob=None):
-		# Removes all non-alphanumeric, non-white space characters
-		query = re.sub(r'[^a-zA-Z\d\s:]', '', query)
-		# If there's a birthdate in the query, 19??. Take it and search usernames
-		regexDob = re.compile('19..')
-		dob = [string for string in query.split() if re.match(regexDob, string)]
-		# Lets only use one birthdate... Change this later
-		if dob:
-			dob = dob[0]
-			query = query.replace(dob,"")
-		print query
+    def __init__(self, query, dob=None):
+        # Removes all non-alphanumeric, non-white space characters
+        query = re.sub(r'[^a-zA-Z\d\s:]', '', query)
+        # If there's a birthdate in the query, 19??.
+        # Take it and search usernames
+        regexDob = re.compile('19..')
+        dob = [string for string in query.split() if re.match(regexDob, string)]
+        # Lets only use one birthdate... Change this later
+        if dob:
+            dob = dob[0]
+            query = query.replace(dob, "")
+        print query
 
-		"""
-		# adding flags.
-			self.sections= query.split(',')
-			for i in self.sections:
-				flagKey = self.sections.split('--')[1]
-				flagValue = self.sections.split('--')[0]
-			if flagKey == 'user':
-		"""
+        """
+        # adding flags.
+            self.sections= query.split(',')
+            for i in self.sections:
+                flagKey = self.sections.split('--')[1]
+                flagValue = self.sections.split('--')[0]
+            if flagKey == 'user':
+        """
 
-		self.dob = dob
-		self.query = query
-		leads = self.nameChk()
-		self.output = self.checkSites(leads)
+        self.dob = dob
+        self.query = query
+        leads = self.nameChk()
+        self.output = self.checkSites(leads)
 
-	def nameChk(self):
-		usernames = []
-		fullnames = []
-		fullnames.append(self.query)
+    def nameChk(self):
+        usernames = []
+        fullnames = []
+        fullnames.append(self.query)
 
-		# If there's only one name in the list... 
-		if len(self.query.split(' ')) == 1:
-			return fullnames
-		
-		for i in fullnames:
-			nameList=i.split()
-			firstLetter = nameList[0][0]
-			firstName = nameList[0]
-			lastName = nameList[-1]
-			lastLetter = nameList[-1][0]
+        # If there's only one name in the list...
+        if len(self.query.split(' ')) == 1:
+            return fullnames
+        for i in fullnames:
+            nameList = i.split()
+            firstLetter = nameList[0][0]
+            firstName = nameList[0]
+            lastName = nameList[-1]
+            lastLetter = nameList[-1][0]
+            # If a middlename exists, lets add it to the mix
+            mid = nameList[1:-1]
+            if mid:
+                middleName = ', '.join(mid)
+                middleLetter = ', '.join(mid)
+                usernames.append(lastName+middleName+firstName)
+                usernames.append(lastName+middleLetter+firstName)
+            # If Date of Birth is a parameter, lets add that as well
+            if self.dob:
+                dob = self.dob
+                dobLast2 = self.dob[-2:]
+                usernames.append(firstName+lastName+dob)
+                usernames.append(firstLetter+lastName+dob)
+                usernames.append(firstName+lastName+dobLast2)
+                usernames.append(firstLetter+lastName+dobLast2)
+            # General parameters that should exist in the original query
+            if nameList > 1:
+                usernames.append(firstLetter+lastName)
+                usernames.append(firstName+lastName)
+                usernames.append(lastName+firstName)
+            if nameList <= 1:
+                usernames.append(firstName)
+        # Return the list of potential usernames from the above
+        self.usernames = usernames
+        usernames = set(usernames)
+        return usernames
 
-			# If a middlename exists, lets add it to the mix
-			mid = nameList[1:-1]
-			if mid:
-				middleName = ', '.join(mid)
-				middleLetter = ', '.join(mid)
-				usernames.append(lastName+middleName+firstName)
-				usernames.append(lastName+middleLetter+firstName)
+    def response200(self, socialList, website, username, socialName='site'):
+        """
+        Check if profiles exist, beware of rate limiting.
+        """
+        website = website + str(username)
+        httpResp = requests.get(
+                    website, stream=True
+                )
+        # Old Request method
+        # httpResp = requests.head(website,stream=True, allow_redirects=True)
+        if httpResp.status_code == 200:
+            socialList.append(website)
+            print 'Potential %s Found...' % socialName
+        else:
+            print 'Search Fail...'
 
-			# If Date of Birth is a parameter, lets add that as well
-			if self.dob:
-				dob = self.dob
-				dobLast2 = self.dob[-2:]
-				usernames.append(firstName+lastName+dob)
-				usernames.append(firstLetter+lastName+dob)
-				usernames.append(firstName+lastName+dobLast2)
-				usernames.append(firstLetter+lastName+dobLast2)
+    def dark200(self, socialList, username):
+        pass
 
-			# General parameters that should exist in the original query
-			if nameList>1:
-				usernames.append(firstLetter+lastName)
-				usernames.append(firstName+lastName)
-				usernames.append(lastName+firstName)
-			if nameList<=1:
-				usernames.append(firstName)
-					
-		# Return the list of potential usernames from the above
-		self.usernames = usernames
-		usernames = set(usernames)
-		return usernames
+    def imageResuts(self, links):
+        """Scrape all of the profile images on a webpage."""
+        pass
 
-	def response200(self,socialList, website, username, socialName='site'):
-		"""
-		Check if profiles exist, beware of rate limiting.
-		"""
-		website = website + str(username)
-		#Old Request method
-		httpResp = requests.get(website, stream=True)#, timeout=(connect_timeout=1, read_timeout=5.0))
-		#httpResp = requests.head(website,stream=True, allow_redirects=True)
-		if httpResp.status_code == 200:
-			socialList.append(website)
-			print 'Potential %s Found...' % socialName 
-		else:
-			print 'Search Fail...'
+    def checkSites(self, usernames):
+        """
+        Save site status to Array.
+        """
+        output = []
+        self.twitter = []
+        self.facebook = []
+        self.youtube = []
+        self.linkedin = []
+        self.github = []
+        self.instagram = []
+        self.gplus = []
+        self.torLinks = []
+        self.torResults = []
 
-	def dark200(self, socialList, username):
-		pass
+        for i in usernames:
+            output.append(i)
+            # Check potential social media using the response200() method
+            self.response200(self.twitter, 'http://twitter.com/', i, 'Twitter')
+            self.response200(self.facebook, 'http://facebook.com/', i, 'Facebook')
+            self.response200(self.youtube, 'https://youtube.com/user/', i, 'YouTube')
+            self.response200(self.linkedin, 'http://linkedin.com/in/', i, 'LinkedIn')
+            self.response200(self.github, 'http://github.com/', i, 'GitHub')
+            self.response200(self.instagram, 'http://instagram.com/', i, 'Instagram')
+            self.response200(self.gplus, 'http://plus.google.com/+', i, 'Google')
 
-	def imageResuts(self, links):
-		"""Scrape all of the profile images on a webpage."""
-		pass
+        # Search Origininal Query on Dark Web.
+        self.onion_check(self.query, i)
+        return output
 
-	def checkSites(self, usernames):
-		"""
-		Save site status to Array.
-		"""
-		output = []
-		self.twitter = []
-		self.facebook = []
-		self.youtube = []
-		self.linkedin = []
-		self.github = []
-		self.instagram = []
-		self.gplus = []
-		self.torLinks = []
-		self.torResults = []
+    def onion_check(self, query, alias):
+        """
+        Temporary: Checks ahmia for onions.
+        """
+        page = requests.get('https://ahmia.fi/search/?q=%s' % query)
+        tree = html.fromstring(page.content)
+        results = tree.xpath('.//cite/text()')
+        print results
+        for i in results:
+            self.torLinks.append('http://%s' % i)
 
-		for i in usernames:
-			output.append(i)
-			# Check potential social media using the response200() method
-			self.response200(self.twitter, 'http://twitter.com/', i, 'Twitter')
-			self.response200(self.facebook, 'http://facebook.com/', i, 'Facebook')
-			self.response200(self.youtube, 'https://youtube.com/user/', i, 'YouTube')
-			self.response200(self.linkedin, 'http://linkedin.com/in/', i, 'LinkedIn')
-			self.response200(self.github, 'http://github.com/', i, 'GitHub')
-			self.response200(self.instagram, 'http://instagram.com/', i, 'Instagram')
-			self.response200(self.gplus, 'http://plus.google.com/+', i, 'Google')
+    def searchResults(self, socialName, image, link, category='website'):
+        """
+        Display each item as an HTML <li> for search.html
+        Gets called in darkmain.py.
+        """
+        lowerName = socialName.lower()
+        hrefs = ""
+        for i in link:
+            nLink = "<p class=\"description\"> <a href=\"%s\">%s</a> </p>" % (str(i), str(i))
+            hrefs = str(nLink + hrefs)
+        if not link:
+            hrefs = "<p class=\"description\">Potential items not found or are hidden</p>"
+        self.results = "<li> <img src=\"../static/listjs/images/icons/%s.png\" class=\"thumb\" />"
+        "<h4><span class=\"name\">%s</span> <span class=\"category\">%s</span></h4> "
+        "<p class=\"description\"> %s <br></p> </li>" % (image, socialName, category, hrefs)
+        return self.results
 
-		# Search Origininal Query on Dark Web.
-		self.onion_check(self.query, i)
-		return output
+    # Make function that prints the Darkweb results organized by query.
+    def darkSites(self, torResults):
+        pass
 
-	def onion_check(self, query, alias):
-		"""
-		Temporary: Checks ahmia for onions.
-		"""
-		page = requests.get('https://ahmia.fi/search/?q=%s' % query)
-		tree = html.fromstring(page.content)
-		results = tree.xpath('.//cite/text()')
-		print results
-		for i in results:
-			self.torLinks.append('http://%s' % i)
-	
-	def searchResults(self, socialName, image, link, category='website'):
-		"""
-		Display each item as an HTML <li> for search.html
-		Gets called in darkmain.py. 
-		"""
-		lowerName = socialName.lower()
-		hrefs = ""
-		for i in link:
-			nLink = "<p class=\"description\"> <a href=\"%s\">%s</a> </p>" % (str(i), str(i))
-			hrefs = str(nLink + hrefs)
-		if not link:
-			hrefs = "<p class=\"description\">Potential items not found or are hidden</p>"
-		self.results = "<li> <img src=\"../static/listjs/images/icons/%s.png\" class=\"thumb\" /><h4><span class=\"name\">%s</span> <span class=\"category\">%s</span></h4><p class=\"description\"> %s <br></p> </li>" % (image, socialName, category, hrefs)
-		return self.results
+    def resultSize(self):
+        """
+        Generate Number of results
+        """
+        results = (
+                len(self.twitter) +
+                len(self.facebook) +
+                len(self.youtube) +
+                len(self.linkedin) +
+                len(self.github) +
+                len(self.torLinks)
+        )
+        return results
 
-	# Make function that prints the Darkweb results organized by query. 
-	def darkSites(self, torResults):
-		pass
 
-	def resultSize(self):
-		"""
-		Generate Number of results
-		"""
-		results = len(self.twitter) + len(self.facebook) + len(self.youtube) + len(self.linkedin) + len(self.github) + len(self.torLinks)
-		return results
-	
-	
 if __name__ == '__main__':
-	example = BackCheck('John Smith')
-	print example.output
+    example = BackCheck('John Smith')
+    print example.output
--- engine.py
+++ engine.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 import re
 import os
@@ -6,36 +8,35 @@ import pandas as pd
 
 
 class SearchEngine(object):
-	"""
-	First, Tor Spiders collect information in /logs and /data. 
-	Then, Tika, NLP algorithms analyze for pandas/pgres ingestion.
-	The query is then passed here and output to search results.
-	"""
+    """
+    First, Tor Spiders collect information in /logs and /data.
+    Then, Tika, NLP algorithms analyze for pandas/pgres ingestion.
+    The query is then passed here and output to search results.
+    """
 
-	def __init__(self):
-		logPath = os.getcwd()+'/logs/process.csv'
-		with open(logPath) as logs:
-			self.logs = pd.read_csv(logs, header=None, sep='\t')
+    def __init__(self):
+        logPath = os.getcwd()+'/logs/process.csv'
+        with open(logPath) as logs:
+            self.logs = pd.read_csv(logs, header=None, sep='\t')
 
-	def search(self, query):
-		"""
-		Parse a pandas data structure for searchability.
-		"""
-		self.query = query
-		content = self.logs[5]
-		matches = self.logs[content.str.contains(query)]
-		
-		# Columns = [DATE,URL,NAME,SIZE,LANG,CONTENT]
-		self.dates = matches[0]
-		self.urls = matches[1]
-		self.names = matches[2]
-		self.size = matches[3]
-		self.lang = matches[4]
-		self.content = matches[5]
-		self.contentList = self.content.tolist()
-		self.brief = self.get_brief2(query,self.contentList[0],10)
+    def search(self, query):
+        """
+        Parse a pandas data structure for searchability.
+        """
+        self.query = query
+        content = self.logs[5]
+        matches = self.logs[content.str.contains(query)]
+        # Columns = [DATE,URL,NAME,SIZE,LANG,CONTENT]
+        self.dates = matches[0]
+        self.urls = matches[1]
+        self.names = matches[2]
+        self.size = matches[3]
+        self.lang = matches[4]
+        self.content = matches[5]
+        self.contentList = self.content.tolist()
+        self.brief = self.get_brief2(query, self.contentList[0], 10)
 
 
 if __name__ == '__main__':
-	test = SearchEngine()
-	test.search('test') 
+    test = SearchEngine()
+    test.search('test')
