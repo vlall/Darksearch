fe1450d7f6e40ed4cca4fa3d70edbdd33788d034
Vishal Lall <vishallall@live.com>
2016-01-28 18:34:40 -0500

    Elastic Search capabilities

--- darksearch-app/tools/elas.py
+++ darksearch-app/tools/elas.py
@@ -1,14 +1,57 @@
-from elasticsearch import Elasticsearch
+#!/usr/bin/python
 
-# by default we don't sniff, ever
+import os
+import pandas as pd
+import json
+from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
-# you can specify to sniff on startup to inspect the cluster and load
-# balance across all nodes
-es = Elasticsearch(["seed1", "seed2"], sniff_on_start=True)
 
-# you can also sniff periodically and/or after failure:
-es = Elasticsearch(["seed1", "seed2"],
-          sniff_on_start=True,
-          sniff_on_connection_fail=True,
-          sniffer_timeout=60)
+
+class DarkElastic(object):
+    """
+    Take a DataFrame and turn it into an Elastic Search Index. 
+    """
+
+
+    def __init__(self):
+        logPath = os.getcwd()+'/../logs/process.csv'
+        with open(logPath) as logs:
+            searchIndex = pd.read_csv(
+                                        logs,
+                                        header=None,
+                                        sep='\t',
+                                        names=[
+                                                "DATES",
+                                                "URLS",
+                                                "NAMES",
+                                                "SIZE",
+                                                "LANG",
+                                                "CONTENT"
+                                        ]
+                            )
+        searchIndex = searchIndex.to_json(orient='index')
+        searchIndex = json.loads(searchIndex)
+        doc = searchIndex['0']
+        self.doc = doc
+
+        ##  Ingest...
+        #  res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
+        #  print(res['created'])
+
+        ##  Get it...
+        #  res = es.get(index="test-index", doc_type='tweet', id=1)
+        #  print(res['_source'])
+
+        ##  Refresh
+        es.indices.refresh(index="test-index")
+
+        ##  Elastic Search
+        res = es.search(index="test-index", body={"query": {"match": {'CONTENT':'BTC'}}})
+        print("Got %d Hits:" % res['hits']['total'])
+        for hit in res['hits']['hits']:
+            print("%(DATES)s: %(URLS)s" % hit["_source"])  
+
+
+if __name__ == '__main__':
+    test = DarkElastic()
