4de1c07d9796bd3383129a2278abbf4da1b56c82
Vishal Lall <vishallall@live.com>
2016-02-20 23:48:57 -0500

    add testing

---
+++ dark_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+from darksearch.darkmain import app
+
+if __name__ == "__main__":
+    app.run(
+            host='0.0.0.0',
+            port=80,
+            debug=True,
+            threaded=True
+    )
+    print "Test Success..."
+    
